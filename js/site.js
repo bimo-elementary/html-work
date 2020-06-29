94f95eb1fd140868b0be4549680e5063e02f60a9
bimo <65071078+bimo-elementary@users.noreply.github.com>
2020-06-29 09:56:42 +0800

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,18 @@
+/*
+<!--第十一小组制作！组员：鞠展，王璐塞，徐朝磊
+------js制作王璐塞---=------------------------------->
+ */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
